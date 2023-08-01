85206d8b0b4255ac7aa00e46c1fa7162517c99cd
Luqman <lqman104@gmail.com>
2023-08-02 06:07:40 +0700

    refactor: remove unused modifier

--- uikit/src/main/java/com/luqman/pokedex/uikit/component/ErrorScreenComponent.kt
+++ uikit/src/main/java/com/luqman/pokedex/uikit/component/ErrorScreenComponent.kt
@@ -14,7 +14,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,7 +42,6 @@ fun ErrorScreenComponent(
             modifier = Modifier
                 .widthIn(max = 200.dp, min = 180.dp)
                 .aspectRatio(1f),
-            contentScale = ContentScale.FillHeight,
         )
         Spacer(modifier = Modifier.padding(8.dp))
         Text(
